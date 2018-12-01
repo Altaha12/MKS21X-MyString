b05e12a317d643b287c6ec60b316d67758c31e3f
Altaha12 <ataha1@stuy.edu>
2018-11-30 23:04:44 -0500

    Update MyString.java

--- MyString.java
+++ MyString.java
@@ -2,7 +2,9 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   //constructor
   public MyString(CharSequence s){
-    this.data=s.toCharArray();
+    int nu=s.length();
+    this.data = new char[nu];
+    for(int i=0;i<data.length;i++){data[i]=s.charAt(i);}
   }
   //Methods
   public int length(){
@@ -13,8 +15,25 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
     return this.data[index];
   }
   public CharSequence subSequence(int start, int end){
-    if(start<0||end<0||end>data.length||start>data.length)throw new IndexOutOfBoundException();
+    if(start<0||end<0||end>data.length||start>data.length||start>end)throw new IndexOutOfBoundsException();
+    String ret="";
+    for(int i=0;i<end-start;i++){
+      ret+=data[i+start];
+    }
+    CharSequence s = new MyString(ret);
+    return s;
   }
-  public String toString(){}
-  }
+  public String toString(){
+    String s="";
+    for(int i=0;i<data.length;i++){
+      s+=data[i];
+    }
+    return s;
+ }
+ public int compareTo(CharSequence o){
+   for(int i=0;i<o.length()&&i<this.length();i++){
+     if(!(o.charAt(i)==this.charAt(i)))return this.charAt(i)-o.charAt(i);
+   }
+   return this.length()-o.length();
+ }
 }
